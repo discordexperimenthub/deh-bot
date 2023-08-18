b9b4a8d2dd6a4b70536cb2b1edf79f1a482a69d1
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-08-18 13:00:13 +0300

    message history for automod

--- src/modules/automod.js
+++ src/modules/automod.js
@@ -76,12 +76,13 @@ module.exports = class AutoMod {
      * @param {boolean} rawContent
      */
     async check(message, rawContent = false) {
+        let history = message.channel.messages.cache.toJSON().map(m => m.content);
         let response = (await axios.post('https://beta.purgpt.xyz/openai/chat/completions', {
             model: 'gpt-3.5-turbo-16k',
             messages: [
                 {
                     role: 'system',
-                    content: 'You are AutoMod, a Discord bot that automatically moderates servers. You are currently in a conversation with multiple users. You must respond with JSON format in a code block like this: ```json\n{\n\t"againstRules": false, // whether the message against server rules or not\n\t"onlyWarn": true, // if true, the message won\'t be deleted, just warning\n\t"rule": 1, // the againsted rule index\n\t"reason": "" // if the message against server rules, enter a reason\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"history": [], // the message history before the message\n\t"message": "", // the message the user sent\n\t"channel": "channel-name", // where the message sent\n\t"author": {\n\t\t"id": "123456", // the user\'s id\n\t\t"username": "", // the user\'s username\n\t}\n}\n```\n\nYou can mention users following <@id> format, like <@12345>.'
+                    content: 'You are AutoMod, a Discord bot that automatically moderates servers. You are currently in a conversation with multiple users. You must respond with JSON format in a code block like this: ```json\n{\n\t"againstRules": false, // whether the message against server rules or not\n\t"onlyWarn": true, // if true, the message won\'t be deleted, just warning\n\t"rule": 1, // the againsted rule index\n\t"reason": "" // if the message against server rules, enter a reason\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"history": [], // the message history before the message\n\t"messageContent": "", // the message the user sent\n\t"channel": "channel-name", // where the message sent\n\t"author": {\n\t\t"id": "123456", // the user\'s id\n\t\t"username": "", // the user\'s username\n\t}\n}\n```\n\nYou can mention users following <@id> format, like <@12345>.'
                 },
                 {
                     role: 'system',
@@ -89,7 +90,7 @@ module.exports = class AutoMod {
                 },
                 {
                     role: 'user',
-                    content: `\`\`\`json\n{\n\t"history": "${message.channel.messages.cache.toJSON().map(m => m.content)}",\\n\t"message": "${message.content}",\n\t"channel": "${message.channel.name}",\n\t"author": {\n\t\t"id": "${message.author.id}",\n\t\t"username": "${message.author.username}"\n\t}\n}\n\`\`\``
+                    content: `\`\`\`json\n{\n\t"history": "${history}",\\n\t"messageContent": "${message.content}",\n\t"channel": "${message.channel.name}",\n\t"author": {\n\t\t"id": "${message.author.id}",\n\t\t"username": "${message.author.username}"\n\t}\n}\n\`\`\``
                 }
             ],
             overwriteOnError: false
@@ -113,17 +114,19 @@ module.exports = class AutoMod {
             return logger('error', 'AUTOMOD', 'Failed to parse JSON:', error, content);
         };
 
+        let sendData = `{\n\t"rule": "No spam.", // the rule which AutoMod triggered\n\t"channel": "channel-name", // where the message sent\n\t"history": [] // the message history before the message\n\t"messageContent": "", // the blocked message content\n\t"reason": "", // the block reason\n}`;
+
         if (data.againstRules) {
             let response2 = (await axios.post('https://beta.purgpt.xyz/openai/chat/completions', {
                 model: 'gpt-3.5-turbo-16k',
                 messages: [
                     {
                         role: 'system',
-                        content: 'You are AutoMod manager. Your job is checking blocked messages. Do not criticize block reasons, only block purposes. You must respond with JSON format in a code block like this: ```json\n{\n\t"correct": true, // whether the block is correct or not\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"rule": "No spam.", // the rule which AutoMod triggered\n\t"channel": "channel-name", // where the message sent\n\t"message": "", // the blocked message content\n\t"reason": "", // the block reason\n}\n```'
+                        content: `You are AutoMod manager. Your job is checking blocked messages. Do not criticize block reasons, only block purposes. You must respond with JSON format in a code block like this: \`\`\`json\n{\n\t"correct": true, // whether the block is correct or not\n}\n\`\`\`\n\nUser messages will be in the format of: \`\`\`json\n${sendData}\n\`\`\``
                     },
                     {
                         role: 'user',
-                        content: `\`\`\`json\n{\n\t"rule": "${this.data.rules[data.rule - 1]}",\n\t"channel": "${message.channel.name}",\n\t"message": "${message.content}",\n\t"reason": "${data.reason}"\n}\n\`\`\``
+                        content: `\`\`\`json\n{\n\t"rule": "${this.data.rules[data.rule - 1]}",\n\t"channel": "${message.channel.name}",\n\t"history": "${history}"\n\t"messageContent": "${message.content}",\n\t"reason": "${data.reason}"\n}\n\`\`\``
                     }
                 ],
                 overwriteOnError: false
@@ -152,9 +155,9 @@ module.exports = class AutoMod {
                 if (data2.correct) return content;
                 else return `\`\`\`json\n${JSON.stringify({ againstRules: false }, null, 4)}\n\`\`\``
             };
-            if (!data2.correct) return logger('error', 'AUTOMOD', 'AutoMod blocked a message incorrectly.', message, JSON.stringify(data, null, 4), JSON.stringify(data2, null, 4));
+            if (!data2.correct) return logger('error', 'AUTOMOD', 'AutoMod blocked a message incorrectly.', sendData, JSON.stringify(data, null, 4), JSON.stringify(data2, null, 4));
             
-            logger('debug', 'AUTOMOD', 'AutoMod blocked a message correctly.', message, JSON.stringify(data, null, 4), JSON.stringify(data2, null, 4));
+            logger('debug', 'AUTOMOD', 'AutoMod blocked a message correctly.', sendData, JSON.stringify(data, null, 4), JSON.stringify(data2, null, 4));
             
             if (data.onlyWarn) await message.reply(`${data.reason}\n*Powered by purgpt.xyz*`);
             else {
