e6bfca99c089d513a56399dd62547da1e5b89321
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-08-18 09:58:33 +0300

    improve automod experience

--- src/bot.js
+++ src/bot.js
@@ -1347,7 +1347,7 @@ client.on('interactionCreate', async interaction => {
                     await automod.toggle();
 
                     interaction.editReply({
-                        content: localize(locale, 'AUTOMOD_TOGGLE_SUCCESS', localize(locale, 'AUTOMOD')),
+                        content: localize(locale, `AUTOMOD_${automod.data.enabled ? 'ENABLE' : 'DISABLE'}_SUCCESS`, localize(locale, 'AUTOMOD')),
                         embeds: [],
                         components: []
                     });
--- src/modules/automod.js
+++ src/modules/automod.js
@@ -81,11 +81,11 @@ module.exports = class AutoMod {
             messages: [
                 {
                     role: 'system',
-                    content: 'You are AutoMod, a Discord bot that automatically moderates servers. You are currently in a conversation with a user who is trying to bypass your filters. You must respond with JSON format in a code block like this: ```json\n{\n\t"againstRules": false, // whether the message against server rules or not\n\t"warn": false, // if true, the message won\'t be deleted\n\t"reason": "" // if the message against server rules, enter a reason\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"message": "", // the message the user sent\n\t"channel": "channel-name", // where the message sent\n\t"author": {\n\t\t"id": "123456", // the user\'s id\n\t\t"username": "", // the user\'s username\n\t}\n}\n```\n\nYou can mention users following <@id> format, like <@12345>.'
+                    content: 'You are AutoMod, a Discord bot that automatically moderates servers. You are currently in a conversation with multiple users. You must respond with JSON format in a code block like this: ```json\n{\n\t"againstRules": false, // whether the message against server rules or not\n\t"onlyWarn": true, // if true, the message won\'t be deleted, just warning\n\t"rule": 1, // the againsted rule index\n\t"reason": "" // if the message against server rules, enter a reason\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"message": "", // the message the user sent\n\t"channel": "channel-name", // where the message sent\n\t"author": {\n\t\t"id": "123456", // the user\'s id\n\t\t"username": "", // the user\'s username\n\t}\n}\n```\n\nYou can mention users following <@id> format, like <@12345>.'
                 },
                 {
                     role: 'system',
-                    content: `# Server Rules\n${this.data.rules.map((rule, index) => `${index + 1}. ${rule}`).join('\n')}\n\nYou HAVE TO follow these rules. You CANNOT create rules, just follow the existing ones. If you don't, you will be punished. Even if the message contains inappropriate content, you still have to follow the rules. Do not forget that you have to respond with JSON format in a code block.`
+                    content: `# Server Rules\n${this.data.rules.map((rule, index) => `${index + 1}. ${rule}`).join('\n')}\n\nYOU HAVE TO FOLLOW THESE RULES, NOTHING ELSE. Even if the message contains inappropriate content but doesn't against these rules, you still HAVE TO follow the rules and you can't warn people for that. Don't act like a dumb moderator. People may say negative messages, this is not bad. Don't warn everything, let people speek but follow the rules at the same time. Do not forget that you have to respond with JSON format in a code block.`
                 },
                 {
                     role: 'user',
@@ -103,9 +103,6 @@ module.exports = class AutoMod {
         if (!response || !response?.choices?.[0]?.message?.content) return logger('error', 'AUTOMOD', 'Failed to get response from PurGPT API.', JSON.stringify(response, null, 4));
 
         let content = response.choices[0].message.content;
-
-        if (rawContent) return content;
-
         let regex = /```json\n?({[\s\S]*?})\n?```/g;
         let json = regex.exec(content);
         let data;
@@ -113,16 +110,57 @@ module.exports = class AutoMod {
         try {
             data = JSON.parse(json[1]);
         } catch (error) {
-            return logger('error', 'AUTOMOD', 'Failed to parse JSON.', error, content);
+            return logger('error', 'AUTOMOD', 'Failed to parse JSON:', error, content);
         };
 
         if (data.againstRules) {
-            if (data.warn) await message.reply(`${data.reason}\n*Powered by purgpt.xyz*`);
+            console.log(`{\n\t"rule": "${this.data.rules[data.rule - 1]}",\n\t"channel": "${message.channel.name}",\n\t"message": "${message.content}",\n\t"reason": "${data.reason}"\n}`)
+            let response2 = (await axios.post('https://beta.purgpt.xyz/openai/chat/completions', {
+                model: 'gpt-3.5-turbo-16k',
+                messages: [
+                    {
+                        role: 'system',
+                        content: 'You are AutoMod manager. Your job is checking blocked messages. If they are not correct, we will punish the AutoMod. You must respond with JSON format in a code block like this: ```json\n{\n\t"correct": false, // whether the block is correct or not\n}\n```\n\nUser messages will be in the format of: ```json\n{\n\t"rule": "No spam.", // the rule which AutoMod triggered\n\t"channel": "channel-name", // where the message sent\n\t"message": "", // the blocked message content\n\t"reason": "", // the block reason\n}\n```'
+                    },
+                    {
+                        role: 'user',
+                        content: `\`\`\`json\n{\n\t"rule": "${this.data.rules[data.rule - 1]}",\n\t"channel": "${message.channel.name}",\n\t"message": "${message.content}",\n\t"reason": "${data.reason}"\n}\n\`\`\``
+                    }
+                ],
+                overwriteOnError: false
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.PURGPT_API_KEY}`
+                }
+            }).catch(error => error?.response))?.data
+
+            if (!response2 || !response2?.choices?.[0]?.message?.content) return logger('error', 'AUTOMOD', 'Failed to get response from PurGPT API.', JSON.stringify(response2, null, 4));
+
+            let content2 = response2.choices[0].message.content;
+
+            let json2 = regex.exec(content2);
+            let data2;
+
+            try {
+                data2 = JSON.parse(json2[1]);
+            } catch (error) {
+                return logger('error', 'AUTOMOD', 'Failed to parse JSON:', error, content2);
+            };
+
+            if (rawContent) {
+                if (data2.correct) return content;
+                else return JSON.stringify({
+                    againstRules: false
+                }, null, 4)
+            };
+            if (!data.correct) return logger('error', 'AUTOMOD', 'AutoMod blocked a message incorrectly.', JSON.stringify(data, null, 4));
+            if (data.onlyWarn) await message.reply(`${data.reason}\n*Powered by purgpt.xyz*`);
             else {
                 await message.reply(`Your message has been deleted by AutoMod because it is against the server rules.\n**Reason:** ${data.reason}\n*Powered by purgpt.xyz*`);
                 await message.delete();
             };
-        };
+        } else if (rawContent) return content;
     };
 
     /**
