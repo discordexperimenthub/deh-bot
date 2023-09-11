7703aa6e13bc2efe55d2c7f4357f6938149e6462
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2023-09-11 14:30:17 +0300

    Create delete-thread.js

---
+++ src/commands/delete-thread.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction } = require("discord.js");
+
+module.exports = {
+    category: 'General',
+    data: new SlashCommandBuilder()
+    .setName('delete-thread')
+    .setName('alt-başlık-sil')
+    .setDescription("Deletes your thread")
+    .setDescriptionLocalizations({
+        tr: 'Alt başlığını siler'
+    }),
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+
+        if (!interaction.channel.isThread()) return interaction.editReply('This command can only be executed in thread channels.');
+        if (!interaction.channel.ownerId !== interaction.user.id) return interaction.editReply('You can only delete your own theads!');
+
+        interaction.channel.delete();
+    }
+};
